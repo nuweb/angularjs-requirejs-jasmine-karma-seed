a643f381212a9fa4398b06e70ddd4389a93a5c36
Chandra Sekhar Veera <veeracs@gmail.com>
2013-04-23 14:59:40 -0400

    add index, package, server, modify component

---
+++ src/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+var app = express();
+
+// Serve static files
+app.use('/', express.static(__dirname + "/"));
+
+// Serve application API
+/*
+app.get('/api/login', function(req, res){
+	// Query the database for that ID and send the response
+	res.jsonp({ user: 'veeracs' });
+});
+*/
+
+app.listen(9000);
+
+console.log("Listening on port 9000...");
